11c56e5c9c45881b7d51e3d58d1d4bfcc850514d
Dare Tunmise <daretunmise@yahoo.com>
2023-09-26 23:53:26 +0100

    commit meme form and memeData

--- src/Meme.js
+++ src/Meme.js
@@ -1,11 +1,20 @@
+import memesData from "./memesData"
+
 function Meme() {
+
+    function getMemeImage() {   
+        const memesArray = memesData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        const url = memesArray[randomNumber].url;
+        console.log(url);
+    }
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" name="" className="form--input" placeholder="top text" />
                 <input type="text" name="" className="form--input" placeholder="bottom text"/>
-                <button className="form--button">Get a new meme image  🖼</button>
-            </form>
+                <button className="form--button" onClick={getMemeImage}>Get a new meme image  🖼</button>
+            </div>
         </main>
     )
 }
